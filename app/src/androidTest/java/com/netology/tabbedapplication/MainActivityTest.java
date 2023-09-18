a9912f50d9ab9ef4d76e9b17cf2fce70edd6d135
Paul White <paratrak@list.ru>
2023-09-18 18:28:07 +0300

    Init commit

---
+++ app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
@@ -0,0 +1,52 @@
+package com.netology.tabbedapplication;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.section_label), withText("Page: 1"), isDisplayed()));
+        textView.check(matches(withText("Page: 1")));
+
+        ViewInteraction tabView = onView(
+                allOf(withContentDescription("Tab 2"), isDisplayed()));
+        tabView.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.section_label), withText("Page: 2"), isDisplayed()));
+        textView2.check(matches(withText("Page: 2")));
+    }
+}
